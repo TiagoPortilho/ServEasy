969220b2686ba5cd751d15d195661312a5e68d39
Tiago <103337735+TiagoPortilho@users.noreply.github.com>
2025-03-13 16:26:01 -0300

    Design and navigation
    At this point the prototype will come to life

--- src/classes/ServicoCliente.java
+++
@@ -1,8 +0,0 @@
-package classes;
-
-public interface ServicoCliente {
-        Pedido enviarPedido(Cardapio _cardapio,Mesa[][] mesas);
-        void cancelarPedido(Mesa[][] mesas);
-        void pagarMesa(Mesa[][] mesas);
-        void darFeedback(Mesa[][] mesas, Feedback feedbacklist);
-}
