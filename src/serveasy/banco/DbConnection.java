ee139fc9e162acbaa19890fb75d98656c42055f2
Tiago <103337735+TiagoPortilho@users.noreply.github.com>
2025-03-18 16:16:46 -0300

    Db creation and connection

---
+++ src/serveasy/banco/DbConnection.java
@@ -0,0 +1,33 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import serveasy.view.TelaErro;
+
+
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Adm
+ */
+public class DbConnection {
+    
+    public Connection getConnection() {
+        try {
+            Connection conexao = DriverManager.getConnection("jdbc:mysql://localhost/auction_house?useSSL=false", "root", "300807");
+            return conexao;
+        } catch (SQLException ex) {
+            Logger.getLogger(DbConnection.class.getName()).log(Level.SEVERE, null, ex);
+            new TelaErro().setVisible(true);
+        }
+        return null;
+    }
+    
+}
